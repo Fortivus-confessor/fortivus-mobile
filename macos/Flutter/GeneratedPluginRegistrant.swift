cea6646d7132fcdd7c9e3634b86fa54838f2877f
arthconf <arthur-confessor@hotmail.com>
2026-07-01 16:58:44 -0400

    chore(macos): regenera plugin registrant apos troca de dependencias

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -19,7 +19,7 @@ import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
-import sqlite3_flutter_libs
+import sqlcipher_flutter_libs
 import url_launcher_macos
 import webview_flutter_wkwebview
 
